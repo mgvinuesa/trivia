d137ca4f6a55cd6d82e899116b9387f1d84bc233
mgvinuesa <manu_gv84@hotmail.com>
2016-03-12 20:12:53 +0100

    Preparing class tu be tested

---
+++ tests/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -0,0 +1,20 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class GameRunnerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testRun() throws Exception {
+
+    }
+}
